6c7450804453c9977ba6e4dccb1d128d8c9cde1b
csjunior <claudeci.junior@castgroup.com.br>
2022-06-13 07:15:20 -0300

    Projeto01/Estrutura da aplicação/CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { Post } from './Post';
+import { Header } from "./components/Header";
+import './styles.css'
 
-function App() {  
+export function App() {
 
   return (
     <div>
+
+      <Header/>
+
       <Post  
         author="Claudeci Siqueira"
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae sit aut aliquid error natus ea asperiores excepturi culpa rerum fugiat nesciunt officiis, dolorem dolore in ipsa autem dolor, modi soluta."
