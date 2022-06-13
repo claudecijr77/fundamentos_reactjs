9ed52226e9c45822e5978e1de5a3617b300dc7ea
csjunior <claudeci.junior@castgroup.com.br>
2022-06-13 06:15:41 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from './Post';
+
+function App() {  
+
+  return (
+    <div>
+      <Post  
+        author="Claudeci Siqueira"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae sit aut aliquid error natus ea asperiores excepturi culpa rerum fugiat nesciunt officiis, dolorem dolore in ipsa autem dolor, modi soluta."
+      />
+      <Post  
+        author="Angela Lopes"
+        content="Outro post muito legal"
+      />
+    </div>
+  )
+}
+
