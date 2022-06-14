eb1a52e473571693171b885526c9fdf0b8d4bfa5
Claudeci Siqueira de Lucena Júnior <cast.claudeci.utic@sebrae.com.br>
2022-06-14 07:09:53 -0300

    Projeto01/Estrutura da aplicação/Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Post } from './Post';
 import { Header } from "./components/Header";
+
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
 
@@ -8,15 +11,21 @@ export function App() {
     <div>
 
       <Header/>
+      <div className={styles.wrapper}>
+          <Sidebar />
+          <main>
+              <Post
+                  author="Claudeci Siqueira"
+                  content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae sit aut aliquid error natus ea asperiores excepturi culpa rerum fugiat nesciunt officiis, dolorem dolore in ipsa autem dolor, modi soluta."
+              />
+              <Post
+                  author="Angela Lopes"
+                  content="Outro post muito legal"
+              />
+          </main>
+      </div>
 
-      <Post  
-        author="Claudeci Siqueira"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae sit aut aliquid error natus ea asperiores excepturi culpa rerum fugiat nesciunt officiis, dolorem dolore in ipsa autem dolor, modi soluta."
-      />
-      <Post  
-        author="Angela Lopes"
-        content="Outro post muito legal"
-      />
+
     </div>
   )
 }
