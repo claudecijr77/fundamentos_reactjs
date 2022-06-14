0f69fc326df9e03fcca6d61d3abf43d879cec1ed
Claudeci Siqueira de Lucena Júnior <cast.claudeci.utic@sebrae.com.br>
2022-06-14 08:02:55 -0300

    Projeto01/Estrutura da aplicação/Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
     return (
@@ -9,12 +10,18 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar}
+                    src="https://avatars.githubusercontent.com/u/51264088?v=4"
+                />
+
                 <strong>Claudeci Júnior</strong>
                 <span>Web Developer</span>
+
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
