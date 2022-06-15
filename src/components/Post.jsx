baeff8821f8f57ee4c9f5371986238a75cb392c1
Claudeci Siqueira de Lucena Júnior <cast.claudeci.utic@sebrae.com.br>
2022-06-15 07:28:13 -0300

    Projeto01/Estrutura da aplicação/Formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,18 @@ export function Post() {
                 </p>
             </div>
 
+            <form className={styles.commentForm}>
+                <strong>Deixe o seu feedback</strong>
+
+                <textarea
+                    placeholder='Deixe o seu comentário'
+                />
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+
+            </form>
         </article>
     )
 }
